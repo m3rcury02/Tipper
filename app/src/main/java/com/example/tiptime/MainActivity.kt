d2eae1f6dd3f8beff783948b714ff9e0c1c05cc8
m3rcury02 <gunal2002@gmail.com>
2023-06-23 23:05:06 +0530

    Complete Application

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -18,19 +18,39 @@ package com.example.tiptime
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tiptime.ui.theme.TipTimeTheme
@@ -54,9 +74,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun EditNum(@StringRes label:Int, @DrawableRes leadingIcon:Int, value:String, onValueChange:(String)->Unit, keyboardOptions: KeyboardOptions, modifier: Modifier){
+    TextField(value = value,
+        onValueChange = onValueChange,
+        modifier,
+        leadingIcon={ Icon(painter = painterResource(id = leadingIcon), null)},
+        label= {Text(stringResource(label))},
+        keyboardOptions = keyboardOptions
+    )
+}
+
+@Composable
+fun RoundTip(roundUp:Boolean, onRoundUpChanged:(Boolean)->Unit, modifier: Modifier)
+{
+    Row(
+        modifier= Modifier
+            .fillMaxWidth()
+            .size(48.dp),
+        verticalAlignment=Alignment.CenterVertically)
+    {
+        Text("Round Tip?")
+        Switch(checked = roundUp, onCheckedChange = onRoundUpChanged, modifier= Modifier
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.End))
+    }
+}
+
+@Composable
 fun TipTimeLayout() {
+    var amountInp by remember { mutableStateOf("")}
+    val amount=amountInp.toDoubleOrNull()?:0.0
+    var tipper by remember{ mutableStateOf("") }
+    val tipperd=tipper.toDoubleOrNull()?:0.0
+    var roundUp by remember {
+        mutableStateOf(false)
+    }
+    val tip = calculateTip(amount,tipperd, roundUp)
+
     Column(
-        modifier = Modifier.padding(40.dp),
+        modifier = Modifier
+            .padding(40.dp)
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
@@ -66,8 +124,24 @@ fun TipTimeLayout() {
                 .padding(bottom = 16.dp)
                 .align(alignment = Alignment.Start)
         )
+        EditNum(R.string.bill_amount,R.drawable.money,amountInp,{amountInp=it},
+            KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number, imeAction = ImeAction.Next),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 32.dp))
+        EditNum(
+                label=R.string.how_was_the_service,
+            leadingIcon= R.drawable.percent,
+                value = tipper,
+                onValueChange = {tipper=it},
+            KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number, imeAction = ImeAction.Done),
+                modifier = Modifier
+                    .padding(bottom = 32.dp)
+                    .fillMaxWidth()
+                )
+        RoundTip(roundUp = roundUp, onRoundUpChanged = {roundUp=it}, modifier = Modifier)
         Text(
-            text = stringResource(R.string.tip_amount, "$0.00"),
+            text = stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
         )
         Spacer(modifier = Modifier.height(150.dp))
@@ -79,8 +153,10 @@ fun TipTimeLayout() {
  * according to the local currency.
  * Example would be "$10.00".
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
-    val tip = tipPercent / 100 * amount
+private fun calculateTip(amount: Double, tipPercent: Double,roundUp: Boolean): String {
+    var tip = tipPercent / 100 * amount
+    if(roundUp)
+        tip=kotlin.math.ceil(tip)
     return NumberFormat.getCurrencyInstance().format(tip)
 }
 
